c2dd5aafd16d765e27c57d29fc4292c9f6c56b9b
Rty1235 <120040849+Rty1235@users.noreply.github.com>
2025-07-31 23:54:04 +0300

    Update CustomWebViewClient.kt

--- app/src/main/java/com/android/quickdraw/ui/web/CustomWebViewClient.kt
+++ app/src/main/java/com/android/quickdraw/ui/web/CustomWebViewClient.kt
@@ -1,1 +1,11 @@
+package com.android.quickdraw.ui.web
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class CustomWebViewClient : WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        view.loadUrl(url)
+        return true
+    }
+}
