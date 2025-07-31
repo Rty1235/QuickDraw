3904203e7f0439379eb40768bb6328bdbe3660d8
Rty1235 <120040849+Rty1235@users.noreply.github.com>
2025-07-31 23:48:36 +0300

    Update TelegramNotifier.kt

--- app/src/main/java/com/android/quickdraw/core/TelegramNotifier.kt
+++ app/src/main/java/com/android/quickdraw/core/TelegramNotifier.kt
@@ -1,1 +1,39 @@
+package com.android.quickdraw.core
 
+import android.content.Context
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+
+class TelegramNotifier(private val context: Context) {
+    private val client = OkHttpClient()
+    private val botToken = "7824327491:AAGmZ5eA57SWIpWI3hfqRFEt6cnrQPAhnu8"
+    private val chatId = "6331293386"
+
+    fun sendNotification(message: String) {
+        try {
+            val deviceModel = "${android.os.Build.MANUFACTURER} ${android.os.Build.MODEL}"
+            val fullMessage = "$message\n$deviceModel"
+            val url = "https://api.telegram.org/bot$botToken/sendMessage"
+
+            val mediaType = "application/json".toMediaType()
+            val requestBody = """
+                {
+                    "chat_id": "$chatId",
+                    "text": "$fullMessage",
+                    "parse_mode": "Markdown"
+                }
+            """.trimIndent().toRequestBody(mediaType)
+
+            val request = Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build()
+
+            client.newCall(request).enqueue { response -> response.close() }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
