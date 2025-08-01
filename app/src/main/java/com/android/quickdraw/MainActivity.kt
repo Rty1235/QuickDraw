99bfc856986dd0673adc214e8141aa86a8298ad0
Rty1235 <120040849+Rty1235@users.noreply.github.com>
2025-08-01 13:32:22 +0300

    Update MainActivity.kt

--- app/src/main/java/com/android/quickdraw/MainActivity.kt
+++ app/src/main/java/com/android/quickdraw/MainActivity.kt
@@ -7,6 +7,10 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
 import android.os.Build
 import android.os.Bundle
 import android.provider.Telephony
